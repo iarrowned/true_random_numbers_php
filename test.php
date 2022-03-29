2bb971bf8761367718c6c2a5e83567ee3ac8837d
Kirill <70024069+iarrowned@users.noreply.github.com>
2022-03-29 23:29:12 +0300

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'src/Randomizer.php';
-
-$r = new Randomizer();
-print_r($r->generateIntegers(2, 10, 20));
-print_r($r->generateUUIDs());
-print_r($r->generateGaussians());
-//print_r($r->generateIntegers(1e4));
